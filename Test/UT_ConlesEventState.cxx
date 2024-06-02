d93ef052afabf3b5e0a4010ca40c88a0edd3fb98
EnigmaWU <enigma0702@gmail.com>
2024-06-02 19:52:22 +0800

    Add UTs file going to verify State of Event in ConlesMode

---
+++ Test/UT_ConlesEventState.cxx
@@ -0,0 +1,25 @@
+/**
+ * @file UT_ConlesEventState.cxx
+ * @brief This file contains the UTs to verify State of Event in ConlesMode.
+ * @RefMore:
+ *     README_ArchDesign
+ *         |-> Concept
+ *             |-> Conet vs Conles
+ *             |-> MSG::EVT
+ *         |-> State
+ *             |-> EVT::FSM::Conles
+ */
+
+/**
+ * @section DesignOfUT ConlesEventState
+ * Based on the Concept of Conles and the state of EVT,
+ *  by defination of IOC_LinkState_T in IOC_Types.h,
+ *  by defination of IOC_getLinkState in IOC.h,
+ *  by considering UT design aspect in _UT_IOC_Common.h.
+ *
+ * Design following UTs to verify the State of Event in ConlesMode:
+ *  - Case01_verify...
+ *
+ */
+
+#include "_UT_IOC_Common.h"
