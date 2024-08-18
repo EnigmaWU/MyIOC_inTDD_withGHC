b9193fad49857e3e5cae0e60faf10680b4556c47
EnigmaWU <enigma0702@gmail.com>
2024-08-18 15:41:31 +0800

    [RED] verifyForceProcEVT_willWaitForLastBlockedCbProcEvtReturning

--- Test/UT_ConlesEventUseBoundary.cxx
+++ Test/UT_ConlesEventUseBoundary.cxx
@@ -78,3 +78,72 @@ TEST(ConlesEventUseBoundary, verifyPostProcEvtSuccess_by1xSubPostUnsubEvt) {
 
     ASSERT_EQ(privData.CbCnt, 1) << "CbProcEvt is MUST==1, while CbCnt=" << privData.CbCnt;  // KeyVerifyPoint
 }
+
+typedef struct {
+    uint32_t Sleep99MsCnt, Sleep999MsCnt;
+} _Case02_PrivData_T, *_Case02_PrivData_pT;
+
+static IOC_Result_T _Case02_CbProcEvt(IOC_EvtDesc_T *pEvtDesc, void *pCbPrivData) {
+    _Case02_PrivData_pT pPrivData = (_Case02_PrivData_pT)pCbPrivData;
+
+    if (pEvtDesc->EvtID == IOC_EVTID_TEST_SLEEP_99MS) {
+      usleep(99 * 1000);  // 99ms
+      pPrivData->Sleep99MsCnt++;
+    } else if (pEvtDesc->EvtID == IOC_EVTID_TEST_SLEEP_999MS) {
+      usleep(999 * 1000);  // 999ms
+      pPrivData->Sleep999MsCnt++;
+    } else {
+      EXPECT_TRUE(false) << "EvtID is not IOC_EVTID_TEST_SLEEP_99MS or IOC_EVTID_TEST_SLEEP_999MS";
+    }
+
+    return IOC_RESULT_SUCCESS;
+}
+
+TEST(ConlesEventUseBoundary, verifyForceProcEVT_willWaitForLastBlockedCbProcEvtReturning) {
+    //===SETUP===
+    _Case02_PrivData_T privData = {
+        .Sleep99MsCnt  = 0,
+        .Sleep999MsCnt = 0,
+    };
+
+    IOC_EvtID_T SubEvtIDs[]  = {IOC_EVTID_TEST_SLEEP_99MS, IOC_EVTID_TEST_SLEEP_999MS};
+    IOC_SubEvtArgs_T SubArgs = {
+        .CbProcEvt_F = _Case02_CbProcEvt,
+        .pCbPrivData = &privData,
+        .pEvtIDs     = SubEvtIDs,
+        .EvtNum      = IOC_calcArrayElmtCnt(SubEvtIDs),
+    };
+
+    IOC_Result_T Result = IOC_subEVT_inConlesMode(&SubArgs);
+    ASSERT_EQ(Result, IOC_RESULT_SUCCESS) << "IOC_subEVT_inConlesMode failed Result=" << Result;
+
+    //===BEHAVIOR===
+    IOC_EvtDesc_T EvtDesc99Ms = {
+        IOC_EVTID_TEST_SLEEP_99MS,
+    };
+    Result = IOC_postEVT_inConlesMode(&EvtDesc99Ms, NULL);
+    ASSERT_EQ(Result, IOC_RESULT_SUCCESS) << "IOC_postEVT_inConlesMode failed Result=" << Result;
+
+    IOC_EvtDesc_T EvtDesc999Ms = {
+        IOC_EVTID_TEST_SLEEP_999MS,
+    };
+    Result = IOC_postEVT_inConlesMode(&EvtDesc999Ms, NULL);
+    ASSERT_EQ(Result, IOC_RESULT_SUCCESS) << "IOC_postEVT_inConlesMode failed Result=" << Result;
+
+    //===VERIFY===
+    IOC_forceProcEVT();  // force to process event
+
+    ASSERT_EQ(privData.Sleep99MsCnt, 1) << "Sleep99MsCnt is MUST==1, while Sleep99MsCnt="
+                                        << privData.Sleep99MsCnt;  // KeyVerifyPoint
+    ASSERT_EQ(privData.Sleep999MsCnt, 1) << "Sleep999MsCnt is MUST==1, while Sleep999MsCnt="
+                                         << privData.Sleep999MsCnt;  // KeyVerifyPoint
+
+    //===CLEANUP===
+    IOC_UnsubEvtArgs_T UnsubArgs = {
+        .CbProcEvt_F = _Case02_CbProcEvt,
+        .pCbPrivData = &privData,
+    };
+
+    Result = IOC_unsubEVT_inConlesMode(&UnsubArgs);
+    ASSERT_EQ(Result, IOC_RESULT_SUCCESS) << "IOC_unsubEVT_inConlesMode failed Result=" << Result;
+}
