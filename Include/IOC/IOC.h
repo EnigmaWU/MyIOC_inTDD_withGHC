f7f35c4dfc1bffc75d1f8c09297c14713fb8d9bd
EnigmaWU <enigma0702@gmail.com>
2024-11-10 15:12:39 +0800

    在 IOC.h 文件中新增对 IOC_SrvAPI.h 的引用，并调整宏定义的格式以提高可读性

--- Include/IOC/IOC.h
+++ Include/IOC/IOC.h
@@ -2,6 +2,7 @@
 
 #include "IOC_EvtAPI.h"
 #include "IOC_Helpers.h"
+#include "IOC_SrvAPI.h"
 #include "IOC_Types.h"
 
 #ifndef __INTER_OBJECT_COMMUNICATION_H__
@@ -40,14 +41,14 @@ IOC_Result_T IOC_getLinkState(
     /*ARG_OUT*/ IOC_LinkState_pT pLinkMainState,
     /*ARG_OUT_OPTIONAL*/ IOC_LinkSubState_pT pLinkSubState);
 
-#define IOC_BugAbort()                                   \
-  do {                                                   \
-    fprintf(stderr, "BUG: %s:%d\n", __FILE__, __LINE__); \
-    abort();                                             \
-  } while (0)
+#define IOC_BugAbort()                                       \
+    do {                                                     \
+        fprintf(stderr, "BUG: %s:%d\n", __FILE__, __LINE__); \
+        abort();                                             \
+    } while (0)
 //<===Helper APIs for UnitTesting && Debugging==========================================================================
 
 #ifdef __cplusplus
 }
 #endif
-#endif//__INTER_OBJECT_COMMUNICATION_H__
+#endif  //__INTER_OBJECT_COMMUNICATION_H__
