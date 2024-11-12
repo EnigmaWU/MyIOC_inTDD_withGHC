88384666a681355eee0dd60c006258dee14c19ea
EnigmaWU <enigma0702@gmail.com>
2024-11-12 22:30:17 +0800

    更新 IOC_Result.c 文件，优化 IOC_getResultStr 函数中的 switch 语句，移除冗余的 case 条目以提高代码的可读性

--- Source/IOC_Result.c
+++ Source/IOC_Result.c
@@ -1,30 +1,28 @@
 #include <IOC/IOC.h>
 
 const char* IOC_getResultStr(IOC_Result_T Result) {
-  switch (Result) {
-    case IOC_RESULT_SUCCESS:
-      return "IOC_RESULT_SUCCESS";
-    case IOC_RESULT_FAILURE:
-      return "IOC_RESULT_FAILURE";
-    case IOC_RESULT_POSIX_ENOMEM:
-      return "IOC_RESULT_POSIX_ENOMEM";
-    case IOC_RESULT_NOT_IMPLEMENTED:
-      return "IOC_RESULT_NOT_IMPLEMENTED";
-    case IOC_RESULT_NOT_SUPPORT:
-      return "IOC_RESULT_NOT_SUPPORT";
-    case IOC_RESULT_NO_EVENT_CONSUMER:
-      return "IOC_RESULT_NO_EVENT_CONSUMER";
-    case IOC_RESULT_TOO_MANY_EVENT_CONSUMER:
-      return "IOC_RESULT_TOO_MANY_EVENT_CONSUMER";
-    case IOC_RESULT_CONFLICT_EVENT_CONSUMER:
-      return "IOC_RESULT_CONFLICT_EVENT_CONSUMER";
-    case IOC_RESULT_TOO_MANY_QUEUING_EVTDESC:
-      return "IOC_RESULT_TOO_MANY_QUEUING_EVTDESC";
-    case IOC_RESULT_INVALID_PARAM:
-      return "IOC_RESULT_INVALID_PARAM";
-    case IOC_RESULT_BUG:
-      return "IOC_RESULT_BUG";
-    default:
-      return "IOC_RESULT_UNKNOWN";
-  }
+    switch (Result) {
+        case IOC_RESULT_SUCCESS:
+            return "IOC_RESULT_SUCCESS";
+        case IOC_RESULT_FAILURE:
+            return "IOC_RESULT_FAILURE";
+        case IOC_RESULT_POSIX_ENOMEM:
+            return "IOC_RESULT_POSIX_ENOMEM";
+        case IOC_RESULT_NOT_IMPLEMENTED:
+            return "IOC_RESULT_NOT_IMPLEMENTED";
+        case IOC_RESULT_NOT_SUPPORT:
+            return "IOC_RESULT_NOT_SUPPORT";
+        case IOC_RESULT_NO_EVENT_CONSUMER:
+            return "IOC_RESULT_NO_EVENT_CONSUMER";
+        case IOC_RESULT_TOO_MANY:
+            return "IOC_RESULT_TOO_MANY";
+        case IOC_RESULT_CONFLICT_EVENT_CONSUMER:
+            return "IOC_RESULT_CONFLICT_EVENT_CONSUMER";
+        case IOC_RESULT_INVALID_PARAM:
+            return "IOC_RESULT_INVALID_PARAM";
+        case IOC_RESULT_BUG:
+            return "IOC_RESULT_BUG";
+        default:
+            return "IOC_RESULT_UNKNOWN";
+    }
 }
