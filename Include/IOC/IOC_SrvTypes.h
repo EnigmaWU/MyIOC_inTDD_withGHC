3eb35d95511e1399c1ad967cb50291e3ab757566
EnigmaWU <enigma0702@gmail.com>
2024-12-08 21:26:16 +0800

    【修改】更新IOC_SrvTypes.h中的注释，明确BROADCAST_EVENT与P2P的区别，并添加IOC_SRVFLAG_BROADCAST_EVENT定义

--- Include/IOC/IOC_SrvTypes.h
+++ Include/IOC/IOC_SrvTypes.h
@@ -81,26 +81,24 @@ typedef enum {
     IOC_SRVFLAG_NONE = 0,
 
     /**
-     * @brief BROADCAST vs P2P==Link2Link
+     * @brief BROADCAST_EVENT vs P2P==Link2Link
      *  P2P means Point to Point, which is a direct link between two objects.
-     *  WHEN service online on server side, it has a SrvID, and MAY get LinkID from this SrvID,
-     *      we name this LinkID as SrvLinkID.
-     *    WHEN client connect, it has a pair of LinkID both in client and server side,
-     *      we name these LinkIDs as ConnLinkID and AcptLinkID.
+     *  WHEN service online on server side, it has a SrvID.
+     *  WHEN client connect, it has a pair of LinkID both in client and server side,
+     *      we name this pair LinkIDs as ConnLinkID and AcptLinkID, or LinkID_atCli and LinkID_atSrv.
      *  ByDefault: Srvice is P2P,
      *      we use AcptLinkID and ConnLinkID to communicate.
-     *  If service is onlined with BROADCAST flag on,
-     *      we use SrvLinkID and ConnLinkID to communicate.
+     *  If service is onlined with BROADCAST flag on, SrvID is aliased to SrvLinkID,
+     *      we use SrvLinkID and ConnLinkIDs to communicate.
      *
      *  Which means:
-     *    <DFT/P2P> AcptLinkID <--> ConnLinkID
+     *    (DFT/P2P) AcptLinkID <--> ConnLinkID
      *          e.g. postEVT(AcptLinkID) --> ONLY ConnLinkID will CbProcEvt
      *          e.g. postEVT(ConnLinkID) --> ONLY AcptLinkID will CbProcEvt
-     *    <BROADCAST> SrvLinkID <--> ConnLinkIDs
+     *    (BROADCAST_EVENT) SrvLinkID <--> ConnLinkIDs
      *          e.g. postEVT(SrvLinkID) --> ALL ConnLinkIDs will CbProcEvt
-     *          e.g. postEVT(ConnLinkID) --> SrvLinkID and ALL OTHER ConnLinkIDs will CbProcEvt
      */
-    // TODO: IOC_SRVFLAG_BROADCAST = 1 << 0,
+    IOC_SRVFLAG_BROADCAST_EVENT = 1 << 0,
 } IOC_SrvFlags_T;
 
 typedef struct {
